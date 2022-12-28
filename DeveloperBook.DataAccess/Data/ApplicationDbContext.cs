0a156acef522aa0c9cb7dc4fe026e9e7894e521a
El Guermai <Anass.ElGuermai@henryschein.com>
2022-12-28 09:54:04 -0700

    adding products

--- DeveloperBook.DataAccess/Data/ApplicationDbContext.cs
+++ DeveloperBook.DataAccess/Data/ApplicationDbContext.cs
@@ -9,14 +9,8 @@ namespace DeveloperBook.DataAccess
         {
 
         }
-
         public DbSet<Category> Categories { get; set; }
-
 		public DbSet<CoverType> CoverTypes { get; set; }
         public DbSet<Product> Products { get; set; }
-
-
-
-        //public DbSet<Product> Products {g,s} 
     }
 }
