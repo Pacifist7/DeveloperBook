6eae141ff4f097fd25a4a509144c2ae09ceef33d
El Guermai <Anass.ElGuermai@henryschein.com>
2023-01-04 13:31:38 -0700

    Lux theme

--- DeveloperBookWeb/Areas/Customer/Controllers/HomeController.cs
+++ DeveloperBookWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using DeveloperBook.DataAccess;
+using DeveloperBook.DataAccess.Repository.IRepository;
 using DeveloperBook.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -10,16 +11,18 @@ namespace DeveloperBookWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork= unitOfWork;
         }
 
         public IActionResult Index()
         {
-
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties:"Category,CoverType");
+            return View(productList);
         }
 
         public IActionResult Privacy()
