1a1769a632169806c76e6435e00179b44ce9d26d
El Guermai <Anass.ElGuermai@henryschein.com>
2023-01-07 16:50:25 -0700

    done with Home Page

--- DeveloperBookWeb/Areas/Customer/Controllers/HomeController.cs
+++ DeveloperBookWeb/Areas/Customer/Controllers/HomeController.cs
@@ -24,6 +24,15 @@ namespace DeveloperBookWeb.Areas.Customer.Controllers
             IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties:"Category,CoverType");
             return View(productList);
         }
+        public IActionResult Details(int id)
+        {
+            ShoppingCart cartObj = new()
+            {
+                Count = 1,
+                Product = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id, includeProperties: "Category,CoverType"),
+            };
+            return View(cartObj);
+        }
 
         public IActionResult Privacy()
         {
